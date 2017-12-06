824313c2613b5096073bfbed61936aee1878bc66
Paolo Calvaresi <paolo.calvaresi.v@gmail.com>
2017-12-06 14:37:02 +0100

    db

--- server/db.js
+++ server/db.js
@@ -1,7 +1,8 @@
 var mongoose = require('mongoose');
-// var dbconnect = process.env.connect || 'mongodb://localhost:27017';
+var remoto = 'mongodb://<dbuser>:<dbpassword>@ds157185.mlab.com:57185/';
+var locale = 'mongodb://localhost:27017';
 mongoose.
-    connect('mongodb://localhost:27017/themoviesdb',
+    connect(locale + '/themoviesdb',
     { useMongoClient: true },
     function (err) {
         if (err) {
